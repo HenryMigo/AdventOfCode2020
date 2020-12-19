2185ffc5b7f42c11779eb924fd68f4eb90b5f1c5
Henry Migo <henryeamigo@gmail.com>
2020-12-19 22:47:10 +0000

    Completed Day Five

---
+++ AdventOfCode.Main/DayFive/DayFive.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Main.DayFive
+{
+    public class DayFive
+    {
+        public IEnumerable<string> ReadFile(string fileLocation)
+        {
+            return File.ReadAllLines(fileLocation);
+        }
+
+        public Tuple<double, double?> Solve(IEnumerable<string> example)
+        {
+            var rowCol = new List<double>();
+
+            foreach (var exam in example)
+            {
+                double row = 0;
+                double col = 0;
+
+                for (var i = 0; i < 7; i++)
+                {
+                    if (exam[i] == 'B')
+                    {
+                        row += Math.Pow(2, 6 - i);
+                    }
+                }
+
+                for (var i = 7; i < 10; i++)
+                {
+                    if (exam[i] == 'R')
+                    {
+                        col += Math.Pow(2, 9 - i);
+                    }
+                }
+
+                var rowId = (row * 8) + col;
+
+                rowCol.Add(rowId);
+            }
+
+            rowCol = rowCol.OrderBy(x => x).ToList();
+
+            var maxModel = rowCol.Select(x => x).Last();
+            double? lastId = -1;
+
+            foreach(double? item in rowCol)
+            {
+                if (lastId != -1 && item - lastId == 2)
+                {
+                    return Tuple.Create(maxModel, item);
+                }
+
+                lastId = item;
+            }
+
+            return Tuple.Create(maxModel, (double?)null);
+        }
+    }
+}
---
+++ AdventOfCodeUnitTests/DayFive_UnitTests.cs
@@ -0,0 +1,60 @@
+﻿using AdventOfCode.Main.DayFive;
+using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AdventOfCodeUnitTests
+{
+    public class DayFive_UnitTests
+    {
+        private const string fileLocation = "D:\\adventOfCode\\2020\\dayfive.txt";
+
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            // B = 1
+            // F = 0
+            // R = 1
+            // L = 0
+            var example = new List<string>
+            {
+                "FBFBBFFRLR"
+            };
+
+            var dayFive = new DayFive();
+
+            // Act
+            var result = dayFive.Solve(example);
+
+            // Assert
+            result.Item1.Should().Be(357);
+        }
+
+        [Fact]
+        public void Test2_WithInput_Part1()
+        {
+            var dayFive = new DayFive();
+            var inputs = dayFive.ReadFile(fileLocation);
+
+            // Act
+            var result = dayFive.Solve(inputs);
+
+            // Assert
+            result.Item1.Should().Be(953);
+        }
+
+        [Fact]
+        public void Test3_WithInput_Part2()
+        {
+            var dayFive = new DayFive();
+            var inputs = dayFive.ReadFile(fileLocation);
+
+            // Act
+            var result = dayFive.Solve(inputs);
+
+            // Assert
+            result.Item2.Should().Be(615);
+        }
+    }
+}
